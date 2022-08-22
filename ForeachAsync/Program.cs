8a7470ab3f88c9da3a10e17510c5e6ff0e5c4411
Fernando Gutierres Damaceno <fernando.damaceno@softplan.com.br>
2022-08-22 20:45:25 -0300

    Demo

---
+++ ForeachAsync/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var lista = new List<int>()
+{
+    1,2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+};
+
+//foreach (var item in lista)
+//{
+//    await Output(item);
+//}
+lista.ForEach(async x => await Output(x));
+Console.WriteLine("fim");
+
+static async Task Output(int i)
+{
+    await Task.Run(() =>
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine(i);
+    });
+}
